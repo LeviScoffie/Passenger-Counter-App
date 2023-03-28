86d3e43608aba8375eaea9d0d40bb52da7e193ea
LeviScoffie <kithinjimiriti4@gmail.com>
2023-03-28 20:56:49 +0300

    create funcs examples

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### PASSENGER COUNTER APP
+## PASSENGER COUNTER APP
 
 * This is the beginning of my learning journey of frontend software engineering for blockchain development.
 
@@ -10,5 +10,36 @@
 **Disclaimer** 
 - This project is a practice project and should not be recognized as one of my portfolio projects..
 
+### Learnings 
+-  Create a function (you decide the name) that logs out the number 42 to the console
+-  Call/invoke the function
+
+``` 
+function num() {
+     console.log(42)
+ }
+
+num()```
+
+-   Create a function that logs out the sum of all the lap times.
+
+```
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function lapsums() {
+let totallaptimes=lap1 +lap2 + lap3
+
+
+console.log(totallaptimes)
+**OR**
+    _console.log(lap1+lap2+lap3)_
+}
+
+lapsums()```
+
+
 
 # LFG!!!!!
+
--- index.js
+++ index.js
@@ -1,37 +1,17 @@
-//document.getElementById("count-el").innerText=5
-let count=5
+
+//// Pseudo-code 
+////initialize the count as 0 
+//// listen for clicks on the increment button
 
 
-// count + 1 incrementing passengers as they come in 
-
-count=count + 1
-
-console.log(count)
+//increment the count varibiale when the button is clicked
+// change the count-el in the HTML to reflect the new count 
 
 
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
-
-let myAge=27 
-
-let humanDogRatio=7 
-
-let myDogAge=myAge * humanDogRatio
-
-console.log(myDogAge)
 
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
- 
-let bonusPoints=50 
 
-bonusPoints=bonusPoints+50 
 
-bonusPoints=bonusPoints-75
 
-bonusPoints=bonusPoints+45
 
-console.log(bonusPoints)
